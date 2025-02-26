ee92d8b53895ff7153249be64e90e970bcb32984
theepicsaxguy <benjamin.sanden@hotmail.com>
2025-02-26 21:31:57 +0100

    feat(k8s): add Longhorn application configuration and update provider versions
    - Introduced a new Longhorn application configuration for ArgoCD
    - Updated Kubernetes provider version constraints across multiple files
    - Adjusted Longhorn default settings for improved performance and reliability

--- tofu/kubernetes/bootstrap/volumes/providers.tf
+++ tofu/kubernetes/bootstrap/volumes/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.35"
+      version = ">=2.36"
     }
     restapi = {
       source  = "Mastercard/restapi"
--- tofu/kubernetes/providers.tf
+++ tofu/kubernetes/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.35"
+      version = "2.36"
     }
     proxmox = {
       source  = "bpg/proxmox"
