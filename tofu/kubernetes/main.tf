16f63e2a3ecf1aaa6e42e8ab6a327bf6cf426f1b
theepicsaxguy <benjamin.sanden@hotmail.com>
2025-03-03 18:34:23 +0100

    chore(k8s): remove cloudflared configuration and resources
    - Deleted cloudflared configuration files and DaemonSet
    - Removed associated namespace and secret definitions

--- tofu/kubernetes/main.tf
+++ tofu/kubernetes/main.tf
@@ -25,84 +25,81 @@ module "talos" {
   }
 
   nodes = {
-    "ctrl-00" = {
-      host_node     = "host3"
-      machine_type  = "controlplane"
-      ip            = "10.25.150.11"
-      mac_address   = "bc:24:11:e6:ba:07"
-      vm_id         = 8101
-      cpu           = 4
-      ram_dedicated = 2096
-      igpu          = false
-    }
-    "ctrl-01" = {
-      host_node     = "host3"
-      machine_type  = "controlplane"
-      ip            = "10.25.150.12"
-      mac_address   = "bc:24:11:44:94:5c"
-      vm_id         = 8102
-      cpu           = 4
-      ram_dedicated = 2096
-      igpu          = false
-      #update        = true
-    }
-    "ctrl-02" = {
-      host_node     = "host3"
-      machine_type  = "controlplane"
-      ip            = "10.25.150.13"
-      mac_address   = "bc:24:11:1e:1d:2f"
-      vm_id         = 8103
-      cpu           = 4
-      ram_dedicated = 2096
-      #update        = true
-    }
-    "work-00" = {
-      host_node     = "host3"
-      machine_type  = "worker"
-      ip            = "10.25.150.21"
-      mac_address   = "bc:24:11:64:5b:cb"
-      vm_id         = 8201
-      cpu           = 4
-      ram_dedicated = 3096
-      disks = {
-        longhorn = {
-          size = "100G"
-          type = "scsi"
-        }
-      }
-    }
-    "work-01" = {
-      host_node     = "host3"
-      machine_type  = "worker"
-      ip            = "10.25.150.22"
-      mac_address   = "bc:24:11:c9:22:c3"
-      vm_id         = 8202
-      cpu           = 4
-      ram_dedicated = 2096
-      disks = {
-        longhorn = {
-          size = "100G"
-          type = "scsi"
-        }
-      }
-    }
-    "work-02" = {
-      host_node     = "host3"
-      machine_type  = "worker"
-      ip            = "10.25.150.23"
-      mac_address   = "bc:24:11:6f:20:03"
-      vm_id         = 8203
-      cpu           = 4
-      ram_dedicated = 2096
-      disks = {
-        longhorn = {
-          size = "100G"
-          type = "scsi"
-        }
+  "ctrl-00" = {
+    host_node     = "host3"
+    machine_type  = "controlplane"
+    ip            = "10.25.150.11"
+    mac_address   = "bc:24:11:e6:ba:07"
+    vm_id         = 8101
+    cpu           = 4
+    ram_dedicated = 4096
+    igpu          = false
+  }
+  "ctrl-01" = {
+    host_node     = "host3"
+    machine_type  = "controlplane"
+    ip            = "10.25.150.12"
+    mac_address   = "bc:24:11:44:94:5c"
+    vm_id         = 8102
+    cpu           = 4
+    ram_dedicated = 4096
+    igpu          = false
+  }
+  "ctrl-02" = {
+    host_node     = "host3"
+    machine_type  = "controlplane"
+    ip            = "10.25.150.13"
+    mac_address   = "bc:24:11:1e:1d:2f"
+    vm_id         = 8103
+    cpu           = 4
+    ram_dedicated = 4096
+  }
+  "work-00" = {
+    host_node     = "host3"
+    machine_type  = "worker"
+    ip            = "10.25.150.21"
+    mac_address   = "bc:24:11:64:5b:cb"
+    vm_id         = 8201
+    cpu           = 4
+    ram_dedicated = 3096
+    disks = {
+      longhorn = {
+        size = "100G"
+        type = "scsi"
       }
     }
   }
-
+  "work-01" = {
+    host_node     = "host3"
+    machine_type  = "worker"
+    ip            = "10.25.150.22"
+    mac_address   = "bc:24:11:c9:22:c3"
+    vm_id         = 8202
+    cpu           = 4
+    ram_dedicated = 4096
+    disks = {
+      longhorn = {
+        size = "100G"
+        type = "scsi"
+      }
+    }
+  }
+  "work-02" = {
+    host_node     = "host3"
+    machine_type  = "worker"
+    ip            = "10.25.150.23"
+    mac_address   = "bc:24:11:6f:20:03"
+    vm_id         = 8203
+    cpu           = 4
+    ram_dedicated = 4096
+    disks = {
+      longhorn = {
+        size = "100G"
+        type = "scsi"
+      }
+    }
+  }
+}
 }
 
 // Removing Proxmox CSI Module
