93d28c72df2602a4e02ad923ca4564c1cfaf044c
theepicsaxguy <benjamin.sanden@hotmail.com>
2025-07-03 20:20:35 +0200

    fix: Remove database seeding from Zigbee2MQTT deployment, add additional secrets for Zigbee network configuration, and clean up kustomization.yaml by removing database.db from configMapGenerator.

--- k8s/applications/automation/zigbee2mqtt/config/ZB30.js
+++ k8s/applications/automation/zigbee2mqtt/config/ZB30.js
@@ -9,11 +9,12 @@ const ea = exposes.access;
 const definition = {
     zigbeeModel: ['MotionSensor-ZB3.0'], // The model ID from: Device with modelID 'lumi.sens' is not supported.
     model: 'S902M-ZG', // Vendor model number, look on the device for a model number
-    vendor: 'HZC  Electric motion sensor', // Vendor of the device (only used for documentation and startup logging)description: 'HZC Electronics motion sensor', // Description of the device, copy from vendor site. (only used for documentation and startup logging)
-    description: 'HZC Electronics motion sensor', // Description of the device, copy from vendor site. (only used for documentation and startup logging)
+    vendor: 'HZC',
+    description: 'Electric motion sensor',
     fromZigbee: [fz.ias_occupancy_alarm_1, fz.battery, fz.illuminance],
     toZigbee: [], // Should be empty, unless device can be controlled (e.g. lights, switches).
     exposes: [e.occupancy(), e.battery_low(), e.battery(), e.illuminance(), e.tamper()], // Defines what this device exposes, used for e.g. Home Assistant discovery and in the frontend
 };
 
-module.exports = definition;
+module.exports = [definition];
+definition.exposes.push(e.linkquality());
