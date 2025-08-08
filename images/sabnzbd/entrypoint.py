9fee7d14b7e2c5d94ef658db7b49cd1b32ba1c78
theepicsaxguy <39008574+theepicsaxguy@users.noreply.github.com>
2025-08-08 15:56:00 +0200

    Update Dockerfile (#1317)
    * Update Dockerfile
    
    * Update entrypoint.py

--- images/sabnzbd/entrypoint.py
+++ images/sabnzbd/entrypoint.py
@@ -1,9 +1,14 @@
 import os
 import shutil
 import sys
+
 config = '/config/sabnzbd.ini'
 default = '/defaults/sabnzbd.ini'
+
+# If PVC is empty, seed with current SAB defaults
 if not os.path.exists(config):
     os.makedirs(os.path.dirname(config), exist_ok=True)
     shutil.copy(default, config)
+
+# Run SABnzbd
 os.execv(sys.executable, [sys.executable, '-m', 'sabnzbd', '--config-file', config])
