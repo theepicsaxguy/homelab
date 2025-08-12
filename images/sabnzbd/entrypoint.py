247b61c0c25b9bcb5cb67b8a4a74354ba0f6f3ee
theepicsaxguy <39008574+theepicsaxguy@users.noreply.github.com>
2025-08-12 12:57:37 +0200

    docs(sabnzbd): explain config subdirectory

--- images/sabnzbd/entrypoint.py
+++ images/sabnzbd/entrypoint.py
@@ -2,14 +2,13 @@ import os
 import shutil
 import sys
 
-config = '/config/sabnzbd.ini'
-default = '/defaults/sabnzbd.ini'
+config_dir = os.environ.get("CONFIG_DIR", "/config/sabnzbd")
+config = os.path.join(config_dir, "sabnzbd.ini")
+default = "/defaults/sabnzbd.ini"
 
-# If PVC is empty, seed with current SAB defaults
 if not os.path.exists(config):
-    os.makedirs(os.path.dirname(config), exist_ok=True)
+    os.makedirs(config_dir, exist_ok=True)
     shutil.copy(default, config)
 
-# Change to SABnzbd directory and run SABnzbd
-os.chdir('/app/sabnzbd')
-os.execv(sys.executable, [sys.executable, '-m', 'sabnzbd', '--config-file', config])
+os.chdir("/app/sabnzbd")
+os.execv(sys.executable, [sys.executable, "-m", "sabnzbd", "--config-file", config])
