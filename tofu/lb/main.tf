289e85430bb1559d1c8b45f3477c62c4c4621e68
theepicsaxguy <39008574+theepicsaxguy@users.noreply.github.com>
2025-08-13 12:56:51 +0200

    feat(tofu): add api load balancer

---
+++ tofu/lb/main.tf
@@ -0,0 +1,83 @@
+locals {
+  control_plane_ips = var.control_plane_ips
+  auth_pass         = "k8sapi"
+  haproxy_cfg = templatefile("${path.module}/templates/haproxy.cfg.tftpl", {
+    control_plane_ips = local.control_plane_ips
+    cluster_domain    = var.cluster_domain
+  })
+}
+
+resource "proxmox_virtual_environment_file" "cloudinit" {
+  for_each     = var.lb_nodes
+  content_type = "snippets"
+  node_name    = each.value.host_node
+  datastore_id = coalesce(each.value.datastore_id, "local")
+  source_raw {
+    data = templatefile("${path.module}/templates/cloud-init.yaml.tftpl", {
+      haproxy_cfg = local.haproxy_cfg
+      keepalived_cfg = templatefile("${path.module}/templates/keepalived.conf.tftpl", {
+        state      = each.key == "lb-00" ? "MASTER" : "BACKUP"
+        priority   = each.key == "lb-00" ? 200 : 150
+        auth_pass  = local.auth_pass
+        api_lb_vip = var.network.api_lb_vip
+      })
+    })
+    file_name = "lb-${each.key}-cloudinit.yaml"
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "lb" {
+  for_each = var.lb_nodes
+
+  node_name   = each.value.host_node
+  name        = each.key
+  description = "K8s API HA LB"
+  tags        = ["k8s", "lb"]
+  vm_id       = each.value.vm_id
+  on_boot     = true
+
+  agent { enabled = true }
+
+  cpu {
+    cores = coalesce(each.value.cpu, 2)
+    type  = "host"
+  }
+  memory { dedicated = coalesce(each.value.ram_dedicated, 2048) }
+
+  network_device {
+    bridge      = var.network.bridge
+    vlan_id     = var.network.vlan_id
+    mac_address = each.value.mac_address
+  }
+
+  disk {
+    datastore_id = coalesce(each.value.datastore_id, var.proxmox_datastore)
+    interface    = "scsi0"
+    ssd          = true
+    size         = 8
+    file_format  = "raw"
+  }
+
+  operating_system { type = "l26" }
+
+  initialization {
+    datastore_id      = coalesce(each.value.datastore_id, var.proxmox_datastore)
+    user_data_file_id = proxmox_virtual_environment_file.cloudinit[each.key].id
+    dns {
+      domain  = var.cluster_domain
+      servers = var.network.dns_servers
+    }
+    ip_config {
+      ipv4 {
+        address = "${each.value.ip}/${var.network.cidr_prefix}"
+        gateway = var.network.gateway
+      }
+    }
+  }
+
+  boot_order = ["scsi0"]
+
+  lifecycle {
+    ignore_changes = [network_device[0].disconnected]
+  }
+}
