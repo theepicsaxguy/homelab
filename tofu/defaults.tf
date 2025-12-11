df2d9bd7de06e6166cadf0552a8b32f22ebbf6cd
theepicsaxguy <39008574+theepicsaxguy@users.noreply.github.com>
2025-12-11 23:35:23 +0100

    fix(tofu): update RAM allocation for worker nodes and add datastore IDs

--- tofu/defaults.tf
+++ tofu/defaults.tf
@@ -19,7 +19,7 @@ variable "defaults_worker" {
     machine_type  = "worker"
     cpu           = 8
     cpu_units     = 1024
-    ram_dedicated = 13312
+    ram_dedicated = 13653
     igpu          = false
     disks = {
       longhorn = {
